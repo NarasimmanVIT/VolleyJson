27881dcd676897b998310cef3bf775de910d9c03
DESKTOP-2BB2BUO\Narasimman <Narasimman315@gmail.com>
2017-12-18 19:22:52 +0530

    comments added

--- app/src/main/java/com/example/narasimman/volley/MainActivity.java
+++ app/src/main/java/com/example/narasimman/volley/MainActivity.java
@@ -21,8 +21,6 @@ import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 
-
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
